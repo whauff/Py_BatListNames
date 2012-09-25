f06de469f81bdc1c9edd89aa36dff1f5139f8f7c
fan  yang <whauff@gmail.com>
2012-09-25 16:50:25 +0800

    测试os模块

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import os
 
+print os.name
+
 PATH = os.getcwd()
 print  PATH
 
 FILES = os.listdir(PATH)
 print  FILES
+
+DIR = 'D:\ptest'
+NEWFILES = os.listdir(DIR)
+print  NEWFILES
+
+for files in NEWFILES:
+    print files
